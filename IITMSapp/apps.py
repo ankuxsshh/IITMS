071d9af567f71c0f3645cd92e51c4a53d292233d
ankuxsshh <ankuxshh72@gmail.com>
2025-01-25 11:51:49 +0530

    first commit

---
+++ IITMSapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IitmsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'IITMSapp'
---
+++ IITMSapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def courses(request):
+    return render(request, 'courses.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
